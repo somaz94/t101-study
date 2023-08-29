bb58e76e481efd1d8d3247bb2def808b905fbea6
somaz <genius5711@gmail.com>
2023-08-29 18:17:14 +0900

    fix:backend.tf.back , dynamodb.tf.back, main.tf

--- 1week/t101-1week-backend-s3-tfstate/main.tf
+++ 1week/t101-1week-backend-s3-tfstate/main.tf
@@ -2,19 +2,19 @@ provider "aws" {
   region = "ap-northeast-2"
 }
 
-# resource "aws_s3_bucket" "tf_state" {
-#   bucket = "somaz-terraform-state-bucket"
+resource "aws_s3_bucket" "tf_state" {
+  bucket = "somaz-terraform-state-bucket"
 
-#   tags = {
-#     Name        = "Terraform State Bucket"
-#     Environment = "Somaz"
-#   }
-# }
+  tags = {
+    Name        = "Terraform State Bucket"
+    Environment = "Somaz"
+  }
+}
 
-# resource "aws_s3_bucket_versioning" "tf_state_versioning" {
-#   bucket = aws_s3_bucket.tf_state.bucket
+resource "aws_s3_bucket_versioning" "tf_state_versioning" {
+  bucket = aws_s3_bucket.tf_state.bucket
 
-#   versioning_configuration {
-#     status = "Enabled"
-#   }
-# }
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
